9090eae1920d750f2336cf932cce7a344d4cb2cc
Theodore Rose <Theodore.O.Rose@gmail.com>
2015-07-16 13:57:29 -0400

    Servlet-ReqSessCon
    Servlet basic example which shows the behavior of Request, Session, and
    Context parameters

---
+++ src/java/com/test/myservlet/MyServlet.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.test.myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author rosth05
+ */
+public class MyServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    String username;
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            username = request.getParameter("username");
+            HttpSession session = request.getSession();
+            ServletContext context = request.getServletContext();
+            
+            if(username != null && username != ""){
+                session.setAttribute("name", username);
+                context.setAttribute("name", username);
+                
+            }
+            out.println("<strong>Request Parameters = "+ username+"</strong><br>");
+            out.println("<strong>Session Parameters = "+ (String)session.getAttribute("name")+"</strong><br>");
+            out.println("<strong>Context Parameters = "+ (String)context.getAttribute("name")+"</strong><br>");
+            
+            
+        
+        }catch(Exception e){
+           
+        }
+               
+        finally {
+            out.close();
+        }
+    }
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : index
+    Created on : Jul 14, 2015, 10:51:34 AM
+    Author     : rosth05
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action="MyServlet" method="get">
+            Username:<input type="text" name="username"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
